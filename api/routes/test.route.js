3ee570e51ff45c4205b4823a1805dd1728dc51a5
Amirhossein Akbari <amirhossein.akbari@dci-student.org>
2026-02-12 20:35:05 +0100

    Remove local env file

--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -6,8 +6,13 @@ import {
 
 const router = express.Router();
 
-router.get("/should-be-logged-in", shouldBeLoggedIn);
+// مسیر ساده برای تست آنلاین بودن Backend
+router.get("/", (req, res) => {
+  res.json({ message: "Backend is online and working!" });
+});
 
+// مسیرهای اصلی با JWT
+router.get("/should-be-logged-in", shouldBeLoggedIn);
 router.get("/should-be-admin", shouldBeAdmin);
 
 export default router;
